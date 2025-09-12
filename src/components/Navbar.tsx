bcc172276971836cfc22b4bdf69185ca64be1448
Agahnim <43116178+naguiagahnim@users.noreply.github.com>
2025-09-12 18:43:37 +0200

    a

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
     return(
-        <nav className="sticky top-0 z-50 bg-white shadow-lg border-b border-slate-200">
+        <nav className="sticky top-0 z-50">
             <h1 className="text-red-500">Navbar</h1>
         </nav>
     )
