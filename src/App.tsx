8a7681d340322ce951c5670c4a2f47b33895ce27
Agahnim <43116178+naguiagahnim@users.noreply.github.com>
2025-09-12 19:27:30 +0200

    push 404 need to fix the routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 import { useState } from 'react'
 import { Home } from "./pages/Home";
 import Navbar from "./components/Navbar";
+import { _404 } from "./pages/_404";
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
     <Navbar/>
     <Routes>
       <Route path="/" element={<Home />}/>
+      <Route path="/404" element={<_404/>}/>
+      <Route index element={<Navigate to="/404" />}/>
     </Routes>
     </>
   )
