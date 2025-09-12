a0a606445591819a40c3ce4897d88bd3d0fa499e
Agahnim <43116178+naguiagahnim@users.noreply.github.com>
2025-09-12 18:41:50 +0200

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import { useState } from 'react'
 import { Test } from "./pages/test";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
+    <>
+    <Navbar/>
     <Routes>
       <Route path="/" element={<Test />}/>
     </Routes>
+    </>
   )
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return(
+        <nav className="sticky top-0 z-50 bg-white shadow-lg border-b border-slate-200">
+            <h1 className="text-red-500">Navbar</h1>
+        </nav>
+    )
+}
