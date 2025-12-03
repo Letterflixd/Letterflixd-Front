f894dceca3c8db111b2a6344f95f5b263886167f
bJacotin <bastien.jacotin@etu.unilim.fr>
2025-12-03 15:14:32 +0100

    add route accueilfilm

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,23 @@ import { Home } from "./pages/Home";
 import Navbar from "./components/Navbar";
 import { _404 } from "./pages/_404";
 import { Details } from "./pages/details.tsx";
-import {Connection} from "./pages/Connection.tsx";
-import {Inscription} from "./pages/Inscription.tsx";
+import { Connection } from "./pages/Connection.tsx";
+import { Inscription } from "./pages/Inscription.tsx";
+import { AccueilFilm } from "./pages/AccueilFilm";
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/404" element={<_404/>}/>
-      <Route path="/details" element={<Details/>}/>
-      <Route path ="*" element={<Navigate to="/404" />}/>
-      <Route path="/connection" element={<Connection />}/>
-      <Route path="/inscription" element={<Inscription />}/>
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/404" element={<_404 />} />
+        <Route path="/details" element={<Details />} />
+        <Route path="*" element={<Navigate to="/404" />} />
+        <Route path="/connection" element={<Connection />} />
+        <Route path="/inscription" element={<Inscription />} />
+        <Route path="/accueilFilm" element={<AccueilFilm />} />
+      </Routes>
     </>
   )
 }
